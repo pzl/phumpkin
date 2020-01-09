6fbc1a4f6c9888a0f1ed38d42d9b8a8c7a49bf4d
pzl <dan@panzarel.la>
2020-01-08 23:25:19 -0500

    make db-writing prep more generic, reusable

--- pkg/photos/database.go
+++ pkg/photos/database.go
@@ -40,46 +40,46 @@ func getValue(tx *badger.Txn, key []byte) ([]byte, error) {
 	return v, nil
 }
 
-func prepXMP(m Meta) ([]byte, []byte, error) {
-	m.EXIF = map[string]interface{}{}
 
-	d, err := json.Marshal(m)
+func marshalForWrite(v interface{}) ([]byte, []byte, error) {
+	d, err := json.Marshal(v)
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "error marshalling XMP to json")
+		return nil, nil, errors.Wrap(err, "error marshalling data to json")
 	}
 
 	t, err := time.Now().MarshalBinary()
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "unable to marshal timestamp for XMP")
+		return nil, nil, errors.Wrap(err, "unable to marshal timestamp for writing db record time")
 	}
 
 	return d, t, nil
 }
 
+type EntrySetter interface {
+	SetEntry(*badger.Entry) error
+}
+
+func writeEntries(e EntrySetter, key string, d []byte, t []byte) error {
+	err := e.SetEntry(badger.NewEntry([]byte(key), d).WithDiscard())
+	if err != nil {
+		return err
+	}
+	err = e.SetEntry(badger.NewEntry([]byte(key+".time"), t).WithDiscard())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func writeXMP(log logrus.FieldLogger, db *badger.DB, file string, m Meta) {
-	d, t, err := prepXMP(m)
+	d, t, err := marshalForWrite(m)
 	if err != nil {
 		log.WithError(err).Error("error preparing XMP for write. Unable to write to DB")
 		return
 	}
 
 	log.WithField("file", file).Trace("writing XMP to db")
-	err = db.Update(func(tx *badger.Txn) error {
-		err := tx.SetEntry(badger.NewEntry([]byte(file+".XMP"), d).WithDiscard())
-		if err != nil {
-			log.WithError(err).Error("unable to set XMP record to db")
-			return err
-		}
-
-		err = tx.SetEntry(badger.NewEntry([]byte(file+".XMP.time"), t).WithDiscard())
-		if err != nil {
-			log.WithError(err).Error("unable to write XMP timestamp to db")
-			return err
-		}
-
-		return nil
-	})
-
+	err = db.Update(func(tx *badger.Txn) error { return writeEntries(tx, file+".XMP", d, t) })
 	if err != nil {
 		log.WithError(err).Error("failed writing XMP to db")
 	} else {
@@ -88,61 +88,32 @@ func writeXMP(log logrus.FieldLogger, db *badger.DB, file string, m Meta) {
 }
 
 func writeXMPBatch(log logrus.FieldLogger, batch *badger.WriteBatch, file string, m Meta) {
-	d, t, err := prepXMP(m)
+	d, t, err := marshalForWrite(m)
 	if err != nil {
 		log.WithError(err).Error("unable to prep XMP for write. Not writing to batch")
 		return
 	}
 
 	log.WithField("file", file).Trace("writing XMP to db-batch")
-	err = batch.SetEntry(badger.NewEntry([]byte(file+".XMP"), d).WithDiscard())
+	err = writeEntries(batch, file+".XMP", d, t)
 	if err != nil {
 		log.WithError(err).Error("unable to add XMP record to batch")
-		return
-	}
-	err = batch.SetEntry(badger.NewEntry([]byte(file+".XMP.time"), t).WithDiscard())
-	if err != nil {
-		log.WithError(err).Error("unable to add XMP time record to batch")
 		return
 	}
 	log.WithField("file", file).Trace("wrote XMP data to batch")
 }
 
-func prepEXIF(e map[string]interface{}) ([]byte, []byte, error) {
-	d, err := json.Marshal(e)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed marshalling exif data to json")
-	}
 
-	t, err := time.Now().MarshalBinary()
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed marshalling exif time")
-	}
-
-	return d, t, nil
-}
 
 func writeEXIF(log logrus.FieldLogger, db *badger.DB, file string, e map[string]interface{}) {
-	d, t, err := prepEXIF(e)
+	d, t, err := marshalForWrite(e)
 	if err != nil {
 		log.WithError(err).Error("unable to prep EXIF data for writing")
 		return
 	}
 
 	log.WithField("file", file).Trace("writing exif to db")
-	err = db.Update(func(tx *badger.Txn) error {
-		err := tx.SetEntry(badger.NewEntry([]byte(file+".EXIF"), d).WithDiscard())
-		if err != nil {
-			return err
-		}
-
-		err = tx.SetEntry(badger.NewEntry([]byte(file+".EXIF.time"), t).WithDiscard())
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
+	err = db.Update(func(tx *badger.Txn) error { return writeEntries(tx, file+".EXIF", d, t) })
 	if err != nil {
 		log.WithError(err).Error("failed writing EXIF to db")
 	} else {
@@ -151,21 +122,16 @@ func writeEXIF(log logrus.FieldLogger, db *badger.DB, file string, e map[string]
 }
 
 func writeEXIFBatch(log logrus.FieldLogger, batch *badger.WriteBatch, file string, e map[string]interface{}) {
-	d, t, err := prepEXIF(e)
+	d, t, err := marshalForWrite(e)
 	if err != nil {
 		log.WithError(err).Error("unable to prep EXIF data for writing to batch")
 		return
 	}
 
 	log.WithField("file", file).Trace("writing EXIF to db-batch")
-	err = batch.SetEntry(badger.NewEntry([]byte(file+".EXIF"), d).WithDiscard())
+	err = writeEntries(batch, file+".EXIF", d, t)
 	if err != nil {
 		log.WithError(err).Error("unable to add EXIF record to batch")
-		return
-	}
-	err = batch.SetEntry(badger.NewEntry([]byte(file+".EXIF.time"), t).WithDiscard())
-	if err != nil {
-		log.WithError(err).Error("unable to add EXIF time record to batch")
 		return
 	}
 	log.WithField("file", file).Trace("wrote EXIF data to batch")
