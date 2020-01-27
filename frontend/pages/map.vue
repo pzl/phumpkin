dcb935ba0d24d86b2b88fd19aa5ef41d645f4e5c
pzl <dan@panzarel.la>
2020-01-27 11:20:43 -0500

    UI: view images on map, and reorder on focus
    closes #3

--- frontend/pages/map.vue
+++ frontend/pages/map.vue
@@ -6,9 +6,20 @@
 				:x="t.x" :y="t.y"
 				:width="t.width" :height="t.height"
 			/>
-			<circle class="photo-spot" v-for="(p,i) in parsed_photos" :key="'photo'+i" :cx="p.position[0]" :cy="p.position[1]" :r="4">
-				<title>{{ p.name }}</title>
-			</circle>
+			<g class="map-photo" v-for="(p,i) in display_photos" :key="p.url" :transform="'translate('+p.position.join(',')+')'" @mouseover="focus_img(p)" @click="focus_img(p)">
+				<circle v-if="scale < 1000" class="photo-spot" :r="4">
+					<title>{{ p.name }}</title>
+				</circle>
+				<g class="photo-view" v-else>
+					<polygon points="-4,-4 4,-4 0,0" :fill="pb" />
+					<g :transform="'translate(-'+pw(p)/2+',-'+(ph(p)+6)+')'">
+						<rect :x="0" :y="0" :width="pw(p)" :height="ph(p)" rx="2" :stroke="pb" />
+						<image :href="p.thumbs['small'].url" :width="pw(p)" :height="ph(p)">
+							<title>{{ p.name }}</title>
+						</image>
+					</g>
+				</g>
+			</g>
 		</svg>
 		<v-select v-model="tile_type" :items="tile_flavors" dense hide-details return-object class="map-type-select" />
 		<div id="attrib">Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a :href="tile_type === 'watercolor' ? 'http://creativecommons.org/licenses/by-sa/3.0' :'http://www.openstreetmap.org/copyright'">{{ tile_type === 'watercolor' ? 'CC BY SA' : 'ODbL' }}</a>.</div>
@@ -73,6 +84,7 @@ export default {
 	computed: {
 		bg() { return this.$vuetify.theme.dark ? '#333' : '#fff' },
 		fg() { return this.$vuetify.theme.dark ? '#4e7372' : '#4e7372' },
+		pb() { return this.$vuetify.theme.dark ? 'black' : '#8c8c8c' }, // photo border color
 		url() {
 			const dpr = window.devicePixelRatio
 			return (x,y,z) => `https://stamen-tiles-${"abc"[Math.abs(x+y) % 3]}.a.ssl.fastly.net/${this.tile_type}/${z}/${x}/${y}${dpr > 1 ? '@2x' : ''}.png`
@@ -108,13 +120,18 @@ export default {
 		path() { return geoPath().projection(this.projection) },
 		parsed_photos() {
 			if (!this.photos) {
-				return null
+				return []
 			}
 			return this.photos.map(p => {
 				p.x = geoConvert(p.meta.loc.lon)
 				p.y = geoConvert(p.meta.loc.lat)
 				p.position = this.projection([p.x,p.y])
 				return p
+			})
+		},
+		display_photos() {
+			return this.parsed_photos.sort((a,b) => {
+				return a.hovered - b.hovered
 			})
 		}
 	},
@@ -123,6 +140,14 @@ export default {
 			this.t = event.transform
 			this.tls = this.tile(event.transform)
 		},
+		pw(photo){ return photo.thumbs['small'].width/2 },
+		ph(photo) { return photo.thumbs['small'].height/2 },
+		focus_img(photo) {
+			const idx = this.photos.indexOf(photo)
+			if (idx !== -1) {
+				this.photos[idx].hovered = new Date()				
+			}
+		}
 	},
 	mounted() {
 		this.width = this.$refs.view.clientWidth
@@ -143,7 +168,7 @@ export default {
 			server = "http://localhost:6001"
 		}
 		this.$axios.get(server + '/api/v1/locations')
-			.then(data => { this.photos = data.data.photos })
+			.then(data => { this.photos = data.data.photos.map(p=>{ p.hovered = 0; return p }) })
 	},
 }
 </script>
@@ -161,9 +186,13 @@ export default {
 	left: 20px;
 }
 
+.photo-view rect {
+	fill: none;
+	stroke-width: 4;
+}
+
 .photo-spot {
 	fill: #ff0000;
-	vector-effect: non-scaling-stroke;
 }
 
 #attrib {
